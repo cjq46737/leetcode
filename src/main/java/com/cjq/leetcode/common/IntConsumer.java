6751cad80e93577e30df6e1b96ab63f111097c2d
陈俊杞 <chenjunqi@thunisoft.com>
2020-11-24 17:21:18 +0800

    增加fizzbuzz得semaphore

---
+++ src/main/java/com/cjq/leetcode/common/IntConsumer.java
@@ -0,0 +1,21 @@
+/*
+ * @projectName leetcode
+ * @package com.cjq.leetcode.common
+ * @className com.cjq.leetcode.common.InuConsumer
+ * @copyright Copyright 2020 Thuisoft, Inc. All rights reserved.
+ */
+package com.cjq.leetcode.common;
+
+/**
+ * InuConsumer
+ * @description TODO
+ * @author chenjunqi
+ * @date 2020年11月24日 15:58
+ * @version 3.0.0
+ */
+public class IntConsumer {
+
+    public void accept(int num) {
+        System.out.print(num + "、");
+    }
+}
